0f90f507ae8edd0a167e0ae518fa82ebf953b7fd
Максим Бельфер <maxbelf1@gmail.com>
2021-03-26 00:11:18 +0300

    Добавлен роутинг

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {MainPage, CartPage} from '../pages';
 import AppHeader from '../app-header';
 import WithRestoService from '../hoc'
+import {Route} from 'react-router-dom'
 
 import Background from './food-bg.jpg';
 
@@ -9,9 +10,9 @@ const App = ({ RestoService }) => {
     console.log(RestoService.getMenuItems())
     return (
         <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
-            <AppHeader total={50}/>
-            <MainPage/>
-            <CartPage/>
+            <AppHeader total={50} />
+            <Route path="/" component={ MainPage} exact/>
+            <Route path="/cart"  component={ CartPage}/>
         </div>
     )
 }
